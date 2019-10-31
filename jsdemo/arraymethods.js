d17ea5c9cd2bfb118d39c01a92540b1066984782
James Walters <james.walters221@gmail.com>
2019-10-31 21:04:30 +0000

    teaching mods

---
+++ jsdemo/arraymethods.js
@@ -0,0 +1,32 @@
+
+
+
+let myArray = [2, 8, 10, 1];
+
+//forEach
+myArray.forEach((x, y) => {
+    console.log(y + ':' + x);
+});
+
+//map
+let myMap = myArray.map((x) => {
+    return x * 3;
+});
+
+//filter
+let myFilter = myArray.filter((number) => {
+    return number >= 8;
+});
+
+//find
+let myFind = myArray.find((y) => {
+    return y > 2;
+});
+
+//reduce
+let add = myArray.reduce((sum, elem) => sum + elem)
+
+console.log(myMap);
+console.log(myFilter);
+console.log(myFind);
+console.log(add);
