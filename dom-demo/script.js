2d28355123a7fb6e170c7bf91a6440df4cf587ef
James Walters <james.walters221@gmail.com>
2019-11-05 20:36:38 +0000

    teaching mods

--- dom-demo/script.js
+++ dom-demo/script.js
@@ -1,82 +1,84 @@
 window.onload = () => {
-  /*
+
+/*
+
   let btnArray = document.querySelectorAll('.btn');
-  
+
   console.log(btnArray);
-  
+
   btnArray.forEach((x, y) => {
     btnArray[y].style.backgroundColor = 'red';
-    btnArray[y].style.Color = 'white';
-  });
-  
-  
-  */
-  
-  document.querySelector('#more-inner-content').innerHTML = "The dom is even more awesome!";
-  
-  /*
-  let body = document.querySelector('body');
-  
-  body.style.backgroundColor = 'red';
-  
-  document.querySelector('#content').innerHTML = "The dom is awesome!";
-  
-  
-  
-  let box = document.querySelectorAll('.box');
-  
-  console.log(box);
-  
-  box.forEach((x) => {
-    
+    btnArray[y].style.color = 'white';
   });
 
-  
+*/
+
+
+  //document.querySelector('#more-inner-content').innerHTML = "The dom is even more awesome!";
+
+  /*
+  let body = document.querySelector('body');
+
+  body.style.backgroundColor = 'red';
+
+  document.querySelector('#content').innerHTML = "The dom is awesome!";
+
+
+
+  let box = document.querySelectorAll('.box');
+
+  console.log(box);
+
+  box.forEach((x) => {
+
+  });
+
+
   let list = document.querySelector('#theList');
-  
+
   //console.log(list);
-  
+
   let listArray = list.childNodes;
-  
+
   listArray.forEach((x) => {
     console.log(x);
   });
-  
-  
+
+
   //document.querySelector('#theList').removeChild(list.childNodes);
-  
- 
+
+
   document.querySelector('#content').innerHTML = "<div>div 1</div><div>div2</div>";
-  
+
   let content = document.querySelector('#content');
-  
+
   content.innerHTML = "The dom is awesome!";
-  
-  
+
+
   content.style.backgroundColor = 'blue';
   content.style.color = '#fff';
-  
-  
+
+
  // let body = document.querySelector('body');
-  
+
   body.style.backgroundColor = 'red';
-  
-  
+
+
   let div = document.querySelectorAll('div');
-  
+
   div.forEach((x, y) => {
     div[y].style.backgroundColor = '#fff';
   });
-  
+
    let greenBackground = () => {
      document.querySelector('body').style.backgroundColor = 'green';
    };
-   
+
    document.querySelector('#btnGreen').addEventListener("click", greenBackground);
-   
-   
-   
+
+
+
    document.querySelector('#btnYellow').addEventListener("click", () => { document.querySelector('body').style.backgroundColor = 'yellow'; });
-   
+
 */
 };
